f2b150d82031e043eb3630d19e7055f5a86b88c7
Brian Mawira <brian.mawi@yahoo.com>
2024-02-14 13:18:16 +0300

    start flow from beginning

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:baus_taka/screens/ecoshop/my_orders_page.dart';
 import 'package:baus_taka/screens/ecoshop/payment_page.dart';
+import 'package:baus_taka/screens/onboarding/splashscreen.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/ecoshop/homepage.dart';
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: EcoHomepage(),
+      home: Splashscreen(),
     );
   }
 }
